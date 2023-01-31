372ece5eea208e41a1afbafd2de60bf05d35d11a
darcy <darcy3057@gmail.com>
2023-01-31 12:15:39 +1100

    Reset it all i guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use leptos::*;
+
+use chargen::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Setup console logging
+    console_error_panic_hook::set_once();
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    // Mount app
+    mount_to_body(|cx| view! { cx,  <App/> })
 }
